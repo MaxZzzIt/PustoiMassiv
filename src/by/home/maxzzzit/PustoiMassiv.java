40306ebb39652659c977993b83e080cef240ddf2
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-11-14 21:02:10 +0300

    Примеры пустых массивов

---
+++ src/by/home/maxzzzit/PustoiMassiv.java
@@ -0,0 +1,18 @@
+package by.home.maxzzzit;
+
+import java.util.Arrays;
+
+public class PustoiMassiv {
+
+	public static void main(String[] args) {
+		int []a;
+		a=new int[8];
+		double [] b = new double[6];
+		boolean [] bul = new boolean[5];
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
+		System.out.println(Arrays.toString(bul));
+
+	}
+
+}
